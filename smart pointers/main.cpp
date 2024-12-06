35c6836a25aa846c4236d1994550c7fa258b90d9
valentinaShturts <valentinashturts@gmail.com>
2024-12-06 11:31:29 +0200

    ff

--- smart pointers/main.cpp
+++ smart pointers/main.cpp
@@ -12,16 +12,11 @@ public:
     MyUniquePtr() : ptr(nullptr) {}
     MyUniquePtr(T* resource) : ptr(resource) {}
 
-    // Забороняємо копіювання
     MyUniquePtr(const MyUniquePtr&) = delete;
     MyUniquePtr& operator=(const MyUniquePtr&) = delete;
 
-    // Дозволяємо переміщення
-    MyUniquePtr(MyUniquePtr&& other) : ptr(other.ptr)
-    {
-        other.ptr = nullptr;
-    }
-    MyUniquePtr& operator=(MyUniquePtr&& other) 
+   
+    MyUniquePtr& Move(MyUniquePtr& other) 
     {
         if (this != &other) 
         {
@@ -32,37 +27,111 @@ public:
         return *this;
     }
 
-    // Оператор розіменування та доступу
     T& operator*() const { return *ptr; }
-    T* operator->() const { return ptr; }
+    T* Get() const { return ptr; }
 
-    // Перевірка на порожній вказівник
     bool isNull() const { return ptr == nullptr; }
 
-    // Звільнення ресурсу
-    void reset(T* resource) 
-    {
-        delete ptr;
-        ptr = resource;
-    }
     void reset()
     {
         delete ptr;
         ptr = nullptr;
     }
-    // Деструктор
+ 
     ~MyUniquePtr() { delete ptr; }
 };
 
+
+
+template <typename T>
+class MySharedPtr 
+{
+private:
+    T* ptr;
+    int* count;
+
+public:
+    MySharedPtr() : ptr(nullptr), count(nullptr) {}
+    MySharedPtr(T* resource) : ptr(resource), count(new int(1)) {}
+
+    MySharedPtr(const MySharedPtr& other) : ptr(other.ptr), count(other.count) 
+    {
+        if (count) ++(*count);
+    }
+
+    MySharedPtr& operator=(const MySharedPtr& other) 
+    {
+        if (this != &other) 
+        {
+            reset(); 
+            ptr = other.ptr;
+            count = other.count;
+            if (count) ++(*count);
+        }
+        return *this;
+    }
+
+    ~MySharedPtr() 
+    {
+        reset();
+    }
+
+    T& operator*() const { return *ptr; }
+    T* Get() const { return ptr; }
+
+    void reset() 
+    {
+        if (count)
+        {
+            --(*count);
+            if (*count == 0)
+            {
+                delete ptr;
+                delete count;
+            }
+        }
+        ptr = nullptr;
+        count = nullptr;
+    }
+
+    int Count() const 
+    {
+        if (count) return *count;
+        else { return 0; }
+    }
+
+    bool isNull() const { return ptr == nullptr; }
+};
+
+
 int main() 
 {
-    MyUniquePtr<int> uptr(new int(42));
-    cout << *uptr << "\n";
+    MyUniquePtr<int> uptr(new int(100));
+    cout << *uptr << "\n"; 
 
-    MyUniquePtr<int> uptr2 = move(uptr); // Переміщення
-    if (uptr.isNull()) cout << "uptr is null\n";
-    cout << *uptr2 << "\n";
-    uptr.reset(new int(100));
-    cout << *uptr << "\n";
+    MyUniquePtr<int> uptr2;
+    uptr2.Move(uptr); 
+    if (uptr.isNull()) cout << "uptr is null\n"; 
+    cout << *uptr2 << "\n"; 
+
+    uptr2.reset();
+    if (uptr2.isNull()) cout << "uptr2 has been reset\n"; 
+
+
+
+    MySharedPtr<int> sp1(new int(100));
+    cout << "Value: " << *sp1 << ", sp1 count: " << sp1.Count() << "\n";
+
+    MySharedPtr<int> sp2 = sp1;
+    cout << "After sp2 copy, sp1 count: " << sp1.Count() << "\n";
+
+    sp2.reset();
+    if (sp2.isNull()) cout << "sp2 is null, sp2 count : " << sp2.Count() << "\n";
+
+ 
+    cout << "After sp2 reset, sp1 count: " << sp1.Count() << "\n";
+
+    sp1.reset(); 
+    cout << "After sp1 reset, sp1 count: " << sp1.Count() << "\n";
     return 0;
 }
